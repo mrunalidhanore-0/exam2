d3c47d32bea370e750e3ff93b680b1a26aa38d2a
mrunalidhanore07 <mrunalidhanore78@gmail.com>
2024-09-06 11:00:27 +0530

    Update q5.c

--- c lang exam2/q5.c
+++ c lang exam2/q5.c
@@ -4,4 +4,26 @@
 void main()
 {
 	char 'a'
+		printf("enter a alp char:");
+	scanf("%c",&a");
+
+	switch(ch)
+	{
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+	 printf("%c is a vowel.\n", ch);
+            break;
+        default:
+           
+            if (ch >= 'a' && ch <= 'z')
+	    {
+                printf("%c is a consonant.\n", ch);
+            } else {
+                printf("%c is not an alphabet character.\n", ch);
+            }
+            break;
+}
 }
